e5034754f04c9a22b53318539731612078684f93
bjih1999 <bjih1999@naver.com>
2023-06-10 15:46:51 +0900

    feat: init

---
+++ src/test/java/com/oceans7/dib/DibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oceans7.dib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DibApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
