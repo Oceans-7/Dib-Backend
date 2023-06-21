5e871ccae4ac3d06c08df2d8e5d9ae90a6c452d5
Jeongminyooa <jmjmjmz732002@naver.com>
2023-06-22 04:07:44 +0900

    test : 지역 코드 조회 API 통신 테스트

--- src/test/java/com/oceans7/dib/openapi/TourAPIServiceTest.java
+++ src/test/java/com/oceans7/dib/openapi/TourAPIServiceTest.java
@@ -1,5 +1,6 @@
 package com.oceans7.dib.openapi;
 
+import com.oceans7.dib.openapi.dto.response.AreaCodeList;
 import com.oceans7.dib.openapi.dto.response.LocationBasedList;
 import com.oceans7.dib.openapi.service.TourAPIService;
 import org.junit.jupiter.api.DisplayName;
@@ -18,7 +19,7 @@ public class TourAPIServiceTest {
 
     @Test
     @DisplayName("위치 기반 서비스 API 통신 테스트")
-    public void callLocationBasedListAPITest() throws IOException {
+    public void callLocationBasedListAPITest() {
         // given
         double mapX = 126.9779692;
         double mapY = 37.566535;
@@ -33,7 +34,7 @@ public class TourAPIServiceTest {
 
     @Test
     @DisplayName("키워드 검색 서비스 API 통신 테스트")
-    public void callSearchKeywordAPITest() throws IOException {
+    public void callSearchKeywordAPITest() {
         // given
         String keyword = "강원";
 
@@ -42,5 +43,17 @@ public class TourAPIServiceTest {
 
         // then
         assertThat(list.getLocationBasedItems().size()).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("지역 코드 조회 API 통신 테스트")
+    public void callAreaCodeAPITest() {
+        String areaCode = "1"; //seoul
+
+        AreaCodeList areaList = tourAPIService.fetchDataFromAreaCodeApi("");
+        AreaCodeList areaDepthList = tourAPIService.fetchDataFromAreaCodeApi(areaCode);
+
+        assertThat(areaList.getAreaCodeItems().size()).isEqualTo(17);
+        assertThat(areaDepthList.getAreaCodeItems().size()).isEqualTo(25);
     }
 }
