f4f56575f378a1fe83b828b55f445dfea3659df5
Jeongminyooa <jmjmjmz732002@naver.com>
2023-06-22 00:28:53 +0900

    test : 키워드 검색 API 통신 테스트

--- src/test/java/com/oceans7/dib/openapi/TourAPIServiceTest.java
+++ src/test/java/com/oceans7/dib/openapi/TourAPIServiceTest.java
@@ -16,7 +16,7 @@ public class TourAPIServiceTest {
 
     @Test
     @DisplayName("위치 기반 서비스 API 통신 테스트")
-    public void callLocationBasedListAPI() throws IOException {
+    public void callLocationBasedListAPITest() throws IOException {
         // given
         double mapX = 126.9779692;
         double mapY = 37.566535;
@@ -24,6 +24,20 @@ public class TourAPIServiceTest {
 
         // when
         String result = tourAPIService.fetchDataFromLocationBasedApi(mapX, mapY, contentTypeId);
+        LocationBasedList list = tourAPIService.parsingJsonObject(result);
+
+        // then
+        assertThat(list.getLocationBasedItems().size()).isEqualTo(10);
+    }
+
+    @Test
+    @DisplayName("키워드 검색 서비스 API 통신 테스트")
+    public void callSearchKeywordAPITest() throws IOException {
+        // given
+        String keyword = "강원";
+
+        // when
+        String result = tourAPIService.fetchDataFromSearchKeywordApi(keyword);
         LocationBasedList list = tourAPIService.parsingJsonObject(result);
 
         // then
