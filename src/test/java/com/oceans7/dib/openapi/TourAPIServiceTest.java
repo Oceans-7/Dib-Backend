096717db1f835b4fe610f877944ad23a495e094e
Jeongminyooa <jmjmjmz732002@naver.com>
2023-06-23 19:41:37 +0900

    test : 지역 기반 관광정보 조회 테스트

--- src/test/java/com/oceans7/dib/openapi/TourAPIServiceTest.java
+++ src/test/java/com/oceans7/dib/openapi/TourAPIServiceTest.java
@@ -1,14 +1,13 @@
 package com.oceans7.dib.openapi;
 
-import com.oceans7.dib.openapi.dto.response.AreaCodeList;
-import com.oceans7.dib.openapi.dto.response.LocationBasedList;
+import com.oceans7.dib.domain.place.ContentType;
+import com.oceans7.dib.openapi.dto.response.simple.AreaCodeList;
+import com.oceans7.dib.openapi.dto.response.simple.TourAPICommonListResponse;
 import com.oceans7.dib.openapi.service.TourAPIService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.io.IOException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -19,17 +18,17 @@ public class TourAPIServiceTest {
 
     @Test
     @DisplayName("위치 기반 서비스 API 통신 테스트")
-    public void callLocationBasedListAPITest() {
+    public void callLocationBasedAPITest() {
         // given
         double mapX = 126.9779692;
         double mapY = 37.566535;
-        String contentTypeId = "12";
+        ContentType contentType = ContentType.TOURIST_SPOT;
 
         // when
-        LocationBasedList list = tourAPIService.fetchDataFromLocationBasedApi(mapX, mapY, contentTypeId);
+        TourAPICommonListResponse list = tourAPIService.fetchDataFromLocationBasedApi(mapX, mapY, contentType);
 
         // then
-        assertThat(list.getLocationBasedItems().size()).isEqualTo(10);
+        assertThat(list.getTourAPICommonItemResponseList().size()).isEqualTo(10);
     }
 
     @Test
@@ -39,10 +38,10 @@ public class TourAPIServiceTest {
         String keyword = "강원";
 
         // when
-        LocationBasedList list = tourAPIService.fetchDataFromSearchKeywordApi(keyword);
+        TourAPICommonListResponse list = tourAPIService.fetchDataFromSearchKeywordApi(keyword);
 
         // then
-        assertThat(list.getLocationBasedItems().size()).isEqualTo(10);
+        assertThat(list.getTourAPICommonItemResponseList().size()).isEqualTo(10);
     }
 
     @Test
@@ -55,5 +54,17 @@ public class TourAPIServiceTest {
 
         assertThat(areaList.getAreaCodeItems().size()).isEqualTo(17);
         assertThat(areaDepthList.getAreaCodeItems().size()).isEqualTo(25);
+    }
+
+    @Test
+    @DisplayName("지역 기반 관광정보 조회 API 통신 테스트")
+    public void callAreaBasedAPITest() {
+        // given : '서울시 강북구' 지역
+        String areaCode = "1";
+        String sigunguCode = "3";
+
+        TourAPICommonListResponse tourAPICommonList = tourAPIService.fetchDataFromAreaBasedApi(areaCode, sigunguCode);
+
+        assertThat(tourAPICommonList.getTourAPICommonItemResponseList().size()).isEqualTo(10);
     }
 }
