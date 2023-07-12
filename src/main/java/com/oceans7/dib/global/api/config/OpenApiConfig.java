4eaff03ec6bad62c4950693906ae76f7dc22072d
Jeongminyooa <jmjmjmz732002@naver.com>
2023-07-12 18:40:33 +0900

    fix : 타임아웃 30초 설정

--- src/main/java/com/oceans7/dib/global/api/config/OpenApiConfig.java
+++ src/main/java/com/oceans7/dib/global/api/config/OpenApiConfig.java
@@ -55,7 +55,7 @@ public class OpenApiConfig {
 
         return HttpServiceProxyFactory
                 .builder(WebClientAdapter.forClient(webClient))
-                .blockTimeout(Duration.ofMillis(7000))
+                .blockTimeout(Duration.ofMillis(30000))
                 .build()
                 .createClient(DataGoKrApi.class);
     }
