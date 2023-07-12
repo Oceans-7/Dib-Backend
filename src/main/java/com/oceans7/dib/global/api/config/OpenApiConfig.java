6f0cfeb622ddf572bc3bc53f369b7030e37d21d7
Jeongminyooa <jmjmjmz732002@naver.com>
2023-07-12 12:07:19 +0900

    fix : WebClient timeout 설정

--- src/main/java/com/oceans7/dib/global/api/config/OpenApiConfig.java
+++ src/main/java/com/oceans7/dib/global/api/config/OpenApiConfig.java
@@ -2,14 +2,22 @@ package com.oceans7.dib.global.api.config;
 
 import com.oceans7.dib.global.api.http.DataGoKrApi;
 import com.oceans7.dib.global.api.http.KakaoApi;
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.support.WebClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class OpenApiConfig {
@@ -25,10 +33,21 @@ public class OpenApiConfig {
 
     private final static String kakaoHeader = "KakaoAK ";
 
+    private HttpClient httpClient() {
+        return HttpClient.create()
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+                .responseTimeout(Duration.ofMillis(5000))
+                .doOnConnected(conn ->
+                        conn.addHandlerLast(new ReadTimeoutHandler(7000, TimeUnit.MILLISECONDS))
+                                .addHandlerLast(new WriteTimeoutHandler(7000, TimeUnit.MILLISECONDS)));
+    }
+
     @Bean
     DataGoKrApi dataGoKrApi() {
+
         WebClient webClient = WebClient.builder()
                 .baseUrl(dataGoKrBaseUrl)
+                .clientConnector(new ReactorClientHttpConnector(httpClient()))
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .build();
 
@@ -40,8 +59,10 @@ public class OpenApiConfig {
 
     @Bean
     KakaoApi kakaoApi() {
+
         WebClient webClient = WebClient.builder()
                 .baseUrl(kakaoBaseUrl)
+                .clientConnector(new ReactorClientHttpConnector(httpClient()))
                 .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                 .defaultHeader(HttpHeaders.AUTHORIZATION, kakaoHeader + kakaoServiceKey)
                 .build();
