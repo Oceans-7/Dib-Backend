64562c03cbc3b50415986270398a25116126c761
bjih1999 <bjih1999@naver.com>
2023-08-14 18:48:46 +0900

    feat: swagger bearer authorization 버튼 추가

--- src/main/java/com/oceans7/dib/global/config/SwaggerConfig.java
+++ src/main/java/com/oceans7/dib/global/config/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package com.oceans7.dib.global.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +18,11 @@ public class SwaggerConfig {
 
         Info info = new Info().title(title).description(description).version(version);
 
-        return new OpenAPI().info(info);
+        return new OpenAPI().info(info).components(
+                new Components().addSecuritySchemes(
+                        "bearer-key",
+                        new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT")
+                )
+        );
     }
 }
