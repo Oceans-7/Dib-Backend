58db8f25ae0be5b0b61bb5bcdbaea1d9d1a66d45
Jeongminyooa <jmjmjmz732002@naver.com>
2023-08-31 22:50:43 +0900

    fix : 스웨거 스키마 설정

--- src/main/java/com/oceans7/dib/domain/event/dto/response/CouponResponseDto.java
+++ src/main/java/com/oceans7/dib/domain/event/dto/response/CouponResponseDto.java
@@ -1,5 +1,6 @@
 package com.oceans7.dib.domain.event.dto.response;
 
+import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AccessLevel;
 import lombok.Getter;
@@ -13,6 +14,7 @@ public class CouponResponseDto {
     @Schema(description = "보유 쿠폰 개수", example = "5")
     private int count;
 
+    @ArraySchema(schema = @Schema(description = "장소 정보", implementation = DetailCouponResponseDto.class))
     private List<DetailCouponResponseDto> couponList;
 
     public static CouponResponseDto of(List<DetailCouponResponseDto> couponList) {
