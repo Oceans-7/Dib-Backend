08f8e27525f6c50a88bcb873f0b38ac7a5de75c9
Jeongminyooa <jmjmjmz732002@naver.com>
2023-09-22 15:46:37 +0900

    fix : base entity null값 저장 수정

--- src/main/java/com/oceans7/dib/DibApplication.java
+++ src/main/java/com/oceans7/dib/DibApplication.java
@@ -3,10 +3,12 @@ package com.oceans7.dib;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
 @SpringBootApplication
+@EnableJpaAuditing
 public class DibApplication {
 
     public static void main(String[] args) {
