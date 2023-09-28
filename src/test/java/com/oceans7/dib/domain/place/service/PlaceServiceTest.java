a9af5f5bc44684cf25890dc7f6d1605598fb166e
JeongMin <yooa.dev@gmail.com>
2023-09-28 21:12:46 +0900

    fix : 테스트 트랜잭션 관리

--- src/test/java/com/oceans7/dib/domain/place/service/PlaceServiceTest.java
+++ src/test/java/com/oceans7/dib/domain/place/service/PlaceServiceTest.java
@@ -17,6 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -24,6 +25,7 @@ import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Transactional
 public class PlaceServiceTest {
 
     @Autowired
