967fcb41aaaa5b7c232793f3f7a5298b0019a788
JeongMin <yooa.dev@gmail.com>
2023-09-30 20:28:03 +0900

    fix : enum 클래스 유효성 검사 오류 수정

--- src/main/java/com/oceans7/dib/domain/place/dto/request/GetPlaceDetailRequestDto.java
+++ src/main/java/com/oceans7/dib/domain/place/dto/request/GetPlaceDetailRequestDto.java
@@ -2,7 +2,6 @@ package com.oceans7.dib.domain.place.dto.request;
 
 import com.oceans7.dib.domain.place.ContentType;
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
 
@@ -17,7 +16,7 @@ public class GetPlaceDetailRequestDto {
     @Schema(description = "컨텐츠 아이디 (관광 리스트 조회시 확인 가능)", example = "126508")
     private Long contentId;
 
-    @NotEmpty
+    @NotNull
     @Schema(description = "컨텐츠 타입 (관광 리스트 조회시 확인 가능)", example = "TOURIST_SPOT")
     private ContentType contentType;
 
